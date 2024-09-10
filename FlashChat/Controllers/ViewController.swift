6de7e1e0f8d67b69619b482a151c261a74082cc9
Miguel Saavedra Lozano <miguel.saavedralozano@garmin.com>
2024-09-10 15:01:20 -0500

    Initial Commit

---
+++ FlashChat/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlashChat
+//
+//  Created by Saavedra Lozano, Miguel on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
